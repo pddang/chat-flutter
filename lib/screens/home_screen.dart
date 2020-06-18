96fd46993d809ce6d71dbd4e6ec4b86f305a3261
Phan Dang <philip.ddang>
2020-06-17 21:53:38 -0700

    content added

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui_starter/models/message_model.dart';
+import 'package:flutter_chat_ui_starter/widgets/category_selector.dart';
+import 'package:flutter_chat_ui_starter/widgets/favorite_contacts.dart';
+import 'package:flutter_chat_ui_starter/widgets/recent_chats.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
+        leading: IconButton(icon: Icon(Icons.menu),
+        iconSize: 30,
+        color: Colors.white,
+        onPressed: (){},),
+        title: Text(
+        'Chats', 
+        style: TextStyle(
+          fontSize: 28,fontWeight: FontWeight.bold),
+          ),
+          elevation: 0.0,
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.search),
+            iconSize: 30,
+            color: Colors.white,
+            onPressed: (){},)
+          ],
+          ),
+          body: Column(children: <Widget>[
+            CategorySelector(),
+            Expanded(
+                          child: Container(
+                decoration: BoxDecoration(color: Theme.of(context).accentColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                )),
+              child: Column(children: <Widget>[
+               FavoriteContacts(),
+               RecentChats(),
+              ],),
+            
+              ),
+            ),
+
+          ],),
+    );
+  }
+}
